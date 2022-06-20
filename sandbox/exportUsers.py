0d7c0845ff130b079d5148b8ffd0566985a15f9e
Amol Deshpande <>
2022-06-20 14:26:42 +0530

    shell scripts

---
+++ sandbox/exportUsers.py
@@ -0,0 +1,12 @@
+import logging
+logging.basicConfig(format="%(asctime)s %(levelname)-7s %(message)s", level=logging.INFO)
+import pandas
+import sys
+sys.path.append('../')
+import apis
+userApi = apis.endpoints.UsersAPI()
+
+usersJSON = userApi.getAllUsers()
+userApi.users2CSV(usersJSON, sys.argv[1])
+usersTable = pandas.read_csv(sys.argv[1])        
+usersTable
