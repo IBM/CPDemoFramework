4676f786d31c502534513de20e38be67ff117cf1
amol-m-deshpande <amol.deshpande@in.ibm.com>
2022-06-22 17:50:07 +0530

    Update github.sh

--- sandbox/github.sh
+++ sandbox/github.sh
@@ -13,6 +13,7 @@ fi
 if [[ $PLAYGROUND_ENVIRONMENT == *"techzone"* ]]; then
       branch="techzone" 
 fi
+
 github_url=https://github.com/IBM/ibmtechzone-demo-artifacts
 project_name=ibmtechzone-demo-artifacts 
 username=IBM
@@ -26,7 +27,7 @@ fi
 # TODO
 # Update the actual url
 cd /projects/techzone-demo/sandbox/$project_name
-git checkout -b $sandbox_username-$demo_name
+# git checkout -b $sandbox_username-$demo_name
 git sparse-checkout set $sandbox_username-$demo_name
 if [ -d "$sandbox_username-$demo_name" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
@@ -53,13 +54,12 @@ git add .
 git commit -am "sandbox demo commit by '$sandbox_username'"
 git push https://user_name:$gtd@github.com/$username/$project_name.git 
 
-curl \
-  -X POST \
-  -H 'Authorization: token '${gtd}'' \
-  https://api.github.com/repos/${username}/${project_name}/pulls \
-  -d '{"title":"Sandbox Demo by '$sandbox_username'","body":"","head":"'$sandbox_username-$demo_name'","base":"'$branch'"}'
+# curl \
+#   -X POST \
+#   -H 'Authorization: token '${gtd}'' \
+#   https://api.github.com/repos/${username}/${project_name}/pulls \
+#   -d '{"title":"Sandbox Demo by '$sandbox_username'","body":"","head":"'$sandbox_username-$demo_name'","base":"'$branch'"}'
 
 cd /projects/techzone-demo/sandbox
 rm -rf $project_name
-echo "Pull Request created successfully!"   
-
+echo "Demo Files Uploaded!"   
