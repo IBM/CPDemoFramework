a733beda1760baa1bf6af4e597ab8a0e9fbf9219
amol-m-deshpande <amol.deshpande@in.ibm.com>
2022-07-22 18:21:21 +0530

    Update env.sh

--- olm-utils-v2/env.sh
+++ olm-utils-v2/env.sh
@@ -59,5 +59,18 @@ while [ "$pod_status" != "True" ] && [ $waittime -lt 300 ];do
     done
 
 
+export CONFIG_DIR=./cpd-config && mkdir -p $CONFIG_DIR/config
+cp cpd-config.yaml $CONFIG_DIR/config
+export STATUS_DIR=./cpd-status && mkdir -p $STATUS_DIR
+
+
 export DEPLOYER_POD=$(oc get po --no-headers -l deployment=cloud-pak-deployer | head -1 | awk '{print $1}')
 oc rsh $DEPLOYER_POD rm -rf /Data/cpd-config && oc cp $CONFIG_DIR $DEPLOYER_POD:/Data/cpd-config/
+
+
+oc rsh deploy/cloud-pak-deployer /cloud-pak-deployer/cp-deploy.sh vault set -vs cp_entitlement_key -vsv "$ICR_KEY"
+
+oc rsh deploy/cloud-pak-deployer /cloud-pak-deployer/cp-deploy.sh vault set \
+  -vs cpd-demo-oc-login -vsv "oc login --server=$SERVER --token=$API_TOKEN"
+
+oc rsh deploy/cloud-pak-deployer /cloud-pak-deployer/cp-deploy.sh vault list
