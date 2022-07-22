1a3fb530a34a716f7896fddc8e2b662bd87b50ce
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-07-22 15:32:56 +0530

    added icr key

--- olm-utils-v2/update-env-vars.py
+++ olm-utils-v2/update-env-vars.py
@@ -3,6 +3,7 @@ server = sys.argv[1]
 api_token = sys.argv[2]
 kubeadmin_user = sys.argv[3]
 kubeadmin_password = sys.argv[4]
+icr_key = sys.argv[5]
 lines = []
 with open("env.sh") as f:
     lines = f.readlines()   
@@ -10,6 +11,7 @@ with open("env.sh") as f:
     lines[4] = api_token + "\n"
     lines[9] = kubeadmin_user + "\n"
     lines[10] = kubeadmin_password + "\n"
+    lines[14] = icr_key + "\n"
     
 with open("env.sh","w") as f:    
     f.writelines(lines)
