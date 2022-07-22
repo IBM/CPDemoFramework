32670f8ee96943371ce7f261b1c62a7f8d0b9a24
Amol Deshpande <>
2022-07-22 13:01:49 +0530

    sandbox temp changes

---
+++ olm-utils-v2/delete.sh
@@ -0,0 +1,1 @@
+oc delete project olm-utils
---
+++ olm-utils-v2/update-env-vars.py
@@ -0,0 +1,15 @@
+import sys
+server = sys.argv[1]
+api_token = sys.argv[2]
+kubeadmin_user = sys.argv[3]
+kubeadmin_password = sys.argv[4]
+lines = []
+with open("env.sh") as f:
+    lines = f.readlines()   
+    lines[1] = server + "\n"
+    lines[4] = api_token + "\n"
+    lines[9] = kubeadmin_user + "\n"
+    lines[10] = kubeadmin_password + "\n"
+    
+with open("env.sh","w") as f:    
+    f.writelines(lines)
