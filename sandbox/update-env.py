68a64f76723f263f1e3f9dc6b02f0823bb436f51
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-08-01 11:27:12 +0530

    cpd cli changes
    Signed-off-by: Suyash Gupte <Suyash.Gupte2@ibm.com>

--- sandbox/update-env.py
+++ sandbox/update-env.py
@@ -3,7 +3,11 @@ import sys
 hostname = sys.argv[1]
 wkcuser=sys.argv[2]
 password=sys.argv[3]
+if(sys.argv[4]):
+    api_key = sys.argv[4]
+else:
+    api_key = "demo_api_key"
 
 f = open("../.env", "w")
-f.write("TZHOSTNAME="+hostname+"\nWKCUSER="+wkcuser+"\nPASSWORD="+password)
+f.write("TZHOSTNAME="+hostname+"\nWKCUSER="+wkcuser+"\nPASSWORD="+password+"\nAPIKEY="+api_key)
 f.close()
