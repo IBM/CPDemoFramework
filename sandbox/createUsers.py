2c64098450c5edab40f507bc698a4da12818315b
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-08-03 19:19:42 +0530

    cpd-cli changes
    Signed-off-by: Suyash Gupte <Suyash.Gupte2@ibm.com>

--- sandbox/createUsers.py
+++ sandbox/createUsers.py
@@ -1,12 +1,21 @@
 import logging
 logging.basicConfig(format="%(asctime)s %(levelname)-7s %(message)s", level=logging.INFO)
-import pandas
 import sys
 sys.path.append('../')
-import apis
 import os
-userApi = apis.endpoints.UsersAPI()
+from decouple import config
 
-os.system('cpd-cli user-mgmt bulk-upsert-users --from-csv-file '+ sys.argv[1] +' --profile sandbox-profile')
+
+CPD_USER_NAME =  config('WKCUSER')
+CPD_USER_PASSWORD =  config('PASSWORD')
+CPD_URL = "https://"+config('TZHOSTNAME')
+CPD_API_KEY = config('APIKEY')
+
+os.system('cpd-cli config users set '+ CPD_USER_NAME +' --username '+ CPD_USER_NAME + ' --apikey '+ CPD_API_KEY)
+
+os.system('cpd-cli config profiles set sandbox-profile --user '+ CPD_USER_NAME + ' --url '+ CPD_URL)
+
+data = os.popen('cpd-cli user-mgmt bulk-upsert-users --from-csv-file '+ sys.argv[1] +' --profile sandbox-profile').read()
+print(data)
 #usersTable = pandas.read_csv(sys.argv[1])        
 #userApi.updateUsersFromTable(usersTable)
--- sandbox/exportUsersv2.py
+++ sandbox/exportUsersv2.py
@@ -3,28 +3,29 @@ logging.basicConfig(format="%(asctime)s %(levelname)-7s %(message)s", level=logg
 import pandas
 import sys
 sys.path.append('../')
-import apis
 import os
 from decouple import config
-
-userApi = apis.endpoints.UsersAPI()
+import json
 
 CPD_USER_NAME =  config('WKCUSER')
 CPD_USER_PASSWORD =  config('PASSWORD')
 CPD_URL = "https://"+config('TZHOSTNAME')
 CPD_API_KEY = config('APIKEY')
+os.system('cpd-cli config users set '+ CPD_USER_NAME +' --username '+ CPD_USER_NAME + ' --apikey '+ CPD_API_KEY)
 
-os.system('cpd-cli config users set sandbox-profile --username'+ CPD_USER_NAME + '--apikey'+ CPD_API_KEY)
-os.system('cpd-cli config profiles set sandbox-profile --username'+ CPD_USER_NAME + '--url'+ CPD_URL)
+os.system('cpd-cli config profiles set sandbox-profile --user '+ CPD_USER_NAME + ' --url '+ CPD_URL)
+usersJSON = json.loads(os.popen("cpd-cli user-mgmt list-users --profile sandbox-profile --output json  ").read())
+print(usersJSON)
 
-data = os.popen("cpd-cli service-instance list --profile sandbox-profile").read()
-print(data)
-usersJSON = userApi.getAllUsers()
 
-for user in usersJSON["UsersInfo"] :
+for user in usersJSON:
     if "email" not in user:
         user["email"] = '--'
 
-userApi.users2CSV(usersJSON, sys.argv[1])
-usersTable = pandas.read_csv(sys.argv[1])        
-usersTable
+     
+usersTable = pandas.DataFrame(columns=['displayName','email','permissions','uid','user_roles','username'])
+
+for user in usersJSON:
+    usersTable.loc[len(usersTable.index)] = [user['displayName'],user['email'],user['permissions'],user['uid'],user['user_roles'],user['username']] 
+print(usersTable)
+usersTable.to_csv(sys.argv[1],index=False)
