f3bf3a5c7e6c2d0b9e95aa3b96ad870490a90114
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-09-05 17:06:58 +0530

    export project using dropdown
    Signed-off-by: Suyash Gupte <Suyash.Gupte2@ibm.com>

--- sandbox/getProjects.py
+++ sandbox/getProjects.py
@@ -3,14 +3,55 @@ import sys
 path = sys.argv[1]
 
 import json
- 
-# Data to be written
-dictionary = {
+import os
+# import platform
+# import requests
+# import tarfile
+# import zipfile
+# from IPython.core.display import display, HTML
+from decouple import config
+
+path = sys.argv[1]
+
+renderData = {
     "parentId" : "project-list",
     "parentTagName": "ul",
     "elementToRender" : "li",
-    "data" : ["Project 1", "Project 2", "Project 3","Project 4"]
+    "data" : []
 }
- 
+# ## CPD Credentials
+
+# In[2]:
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CPD_USER_NAME =  config('WKCUSER')
+CPD_USER_PASSWORD =  config('PASSWORD')
+CPD_URL = "https://"+config('TZHOSTNAME')
+
+version_r = os.popen('cpdctl version').read()
+
+CPDCTL_VERSION = version_r
+CPDCTL_VERSION=CPDCTL_VERSION.strip()
+
+print("cpdctl version: {}".format(CPDCTL_VERSION))
+
+os.system('cpdctl config user set cpd_user --username '+CPD_USER_NAME+ ' --password '+CPD_USER_PASSWORD)
+
+os.system('cpdctl config profile set cpd --url ' +CPD_URL+' --user cpd_user')
+
+os.system('cpdctl config context set cpd --profile cpd --user cpd_user')
+
+os.system('cpdctl config context list')
+
+os.system('cpdctl config context use cpd')
+
+data = json.loads(os.popen("cpdctl project list --output json").read())
+for i in range(0, len(data["resources"])):
+    renderData["data"].append({
+        "name": data['resources'][i]['entity']['name'],
+        "guid": data['resources'][i]['metadata']['guid']
+    })
+
+print(renderData)
 with open(path, "w") as outfile:
-    json.dump(dictionary, outfile)
+    json.dump(renderData, outfile)
