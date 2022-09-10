388e579222afdfea95a3cc6dc06990c95379f8a4
Girish Padmanabhan <pgirish@in.ibm.com>
2022-09-10 19:07:37 +0530

    Import of Private Demo

--- sandbox/getDemoFiles.sh
+++ sandbox/getDemoFiles.sh
@@ -21,13 +21,29 @@ git sparse-checkout set $demo_name
 
 if [ -d "$demo_name" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
-    echo "DemoName Found!"
+    echo "$demo_name DemoName Found!"
     cp -a /projects/techzone-demo/sandbox/$project_name/$demo_name/. /projects/techzone-demo/sandbox/
     rm -rf /projects/techzone-demo/sandbox/$project_name
-    echo "Files loaded successfully!"
+    echo "Demo loaded successfully!"
+
+    isPrivate=`cat /projects/techzone-demo/sandbox/readme.json | jq '.isPrivate'`
+    if [ $isPrivate = "true" ]; then
+      echo "This is a private demo. Lets retrieve the demo files from the private github repo"
+      privateGitRepoUrl=`cat /projects/techzone-demo/sandbox/readme.json | jq '.privateGitRepoUrl'`
+      cd /projects/techzone-demo/sandbox
+      privateGitRepoUrl="${privateGitRepoUrl:1:-1}"
+      git clone --sparse $privateGitRepoUrl $project_name
+      cd /projects/techzone-demo/sandbox/$project_name
+      git sparse-checkout set $demo_name
+      if [ -d "$demo_name" ]; then
+        cp -a /projects/techzone-demo/sandbox/$project_name/$demo_name/* /projects/techzone-demo/sandbox/
+        rm -rf /projects/techzone-demo/sandbox/$project_name
+      else
+        echo "Private Demo not found"
+      fi
+    fi
     exit 2
 fi
 echo "DemoName Not Found!"
 echo "File loading failed!"
 rm -rf /projects/techzone-demo/sandbox/$project_name
-
