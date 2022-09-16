5d65ef2df031d7d0787ef159d714e95373b069f4
Girish Padmanabhan <pgirish@in.ibm.com>
2022-09-16 21:20:55 +0530

    updates to handle git token from UI

--- sandbox/getDemoFiles.sh
+++ sandbox/getDemoFiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
 demo_name=$1
-private_git_token=$2
+isPrivate=$2
+privateGitRepoUrl=$3
+privateGitToken=$4
 project_name=ibmtechzone-demo-artifacts 
 branch="main"
 if [[ $PLAYGROUND_ENVIRONMENT == *"development"* ]]; then
@@ -12,49 +14,34 @@ fi
 if [[ $PLAYGROUND_ENVIRONMENT == *"techzone"* ]]; then
       branch="techzone" 
 fi
-github_url=https://github.com/IBM/ibmtechzone-demo-artifacts
+
 cd /projects/techzone-demo/sandbox
 rm -rf /projects/techzone-demo/sandbox/$project_name
-git clone --single-branch --branch $branch --sparse $github_url 
-cd /projects/techzone-demo/sandbox/$project_name
-git sparse-checkout set $demo_name
-# cd /projects/techzone-demo/sandbox/
 
+if [ $isPrivate == "true" ]; then
+  echo "This is a private demo. Lets retrieve the demo files from the private github repo"
+
+  #If the git token is provided, lets add it to the git url and clone. 
+  if [ "$privateGitToken" ]; then
+    privateGitRepoUrl="${privateGitRepoUrl:0:8}$privateGitToken@${privateGitRepoUrl:8}"
+    git clone --sparse $privateGitRepoUrl $project_name
+    cd /projects/techzone-demo/sandbox/$project_name
+    git sparse-checkout set $demo_name
+  fi
+else 
+  github_url=https://github.com/IBM/ibmtechzone-demo-artifacts
+  echo "This is a public demo. Lets retrieve the demo files from the public github repo"
+  git clone --single-branch --branch $branch --sparse $github_url 
+  cd /projects/techzone-demo/sandbox/$project_name
+  git sparse-checkout set $demo_name
+fi
 if [ -d "$demo_name" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
-    echo "$demo_name DemoName Found!"
-    cp -a /projects/techzone-demo/sandbox/$project_name/$demo_name/. /projects/techzone-demo/sandbox/
-    rm -rf /projects/techzone-demo/sandbox/$project_name
-    echo "Demo loaded successfully!"
-
-    isPrivate=`cat /projects/techzone-demo/sandbox/readme.json | jq '.isPrivate'`
-    if [ $isPrivate = "true" ]; then
-      echo "This is a private demo. Lets retrieve the demo files from the private github repo"
-      privateGitRepoUrl=`cat /projects/techzone-demo/sandbox/readme.json | jq '.privateGitRepoUrl'`
-      cd /projects/techzone-demo/sandbox
-
-      #if privateGitRepoUrl starts with a " remove it. 
-      if [ ${privateGitRepoUrl:0:1}='"' ]; then
-        privateGitRepoUrl="${privateGitRepoUrl:1:-1}"
-      fi
-
-      #If the git token is provided, lets add it to the git url. 
-      if [ "$private_git_token" ]; then
-        private_github_url="${private_github_url:0:8}$private_git_token@${private_github_url:8}"
-      fi
-      git clone --sparse $privateGitRepoUrl $project_name
-      cd /projects/techzone-demo/sandbox/$project_name
-      git sparse-checkout set $demo_name
-      if [ -d "$demo_name" ]; then
-        cp -a /projects/techzone-demo/sandbox/$project_name/$demo_name/* /projects/techzone-demo/sandbox/
-        rm -rf /projects/techzone-demo/sandbox/$project_name
-      else
-        echo "Private Demo not found"
-        exit 2
-      fi
-    fi
-    echo "success"
-    exit 2
+  echo "$demo_name DemoName Found!"
+  cp -a /projects/techzone-demo/sandbox/$project_name/$demo_name/. /projects/techzone-demo/sandbox/
+  rm -rf /projects/techzone-demo/sandbox/$project_name
+  echo "Demo loaded successfully!"
+  echo "success"
+  exit 2
 fi
 echo "DemoName Not Found!"
 echo "File loading failed!"
