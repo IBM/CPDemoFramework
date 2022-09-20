3d3c4291ddea7e20400e488c19d9c26d204198c1
Nupur Negi <nupurn13@gmail.com>
2022-09-20 15:23:43 +0530

    added success message
    Signed-off-by: Nupur Negi <nupurn13@gmail.com>

--- olm-utils-v2/delete.sh
+++ olm-utils-v2/delete.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "Deleting Deployer job..."
 oc delete job -n cloud-pak-deployer cloud-pak-deployer
+if [ "$(oc get job cloud-pak-deployer -n cloud-pak-deployer -o jsonpath='{.status.active}' 2>/dev/null)" != "1" ];then
+    echo "success"
+fi
