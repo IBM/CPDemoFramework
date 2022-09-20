85e839fee4d860fc3585c8b02860371bbb4317e9
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-09-20 08:47:53 +0530

    Update getInstalledServices.py

--- olm-utils-v2/getInstalledServices.py
+++ olm-utils-v2/getInstalledServices.py
@@ -16,41 +16,41 @@ serviceInstalled = {
 
 #####################For cp4d get the installed services#####################
 
-############ Hard coded service list ############
-servicescp4d = {
-    "analyticsengine": 'Analytics Engine Powered by Apache Spark',
-    "bigsql": 'Db2 Big SQL',
-    "ca": 'Cognos Analytics',
-    "cde": 'Cognos Dashboards',
-    "datagate": 'Data Gate',
-    "datastage-ent-plus": 'DataStage Enterprise Plus',
-    "db2": 'Db2',
-    "db2u": 'IBM Db2u',
-    "db2wh": 'Db2 Warehouse',
-    "dmc": 'Data Management Console',
-    "dods": 'Decision Optimization',
-    "dp": 'Data Privacy',
-    "dv": 'Data Virtualization',
-    "hadoop": 'Execution Engine for Apache Hadoop',
-    "mdm": 'IBM Master Data Management',
-    "openpages": 'OpenPages',
-    "planning-analytics": 'Planning Analytics',
-    "rstudio": 'RStudio Server',
-    "spss": 'SPSS Modeler',
-    "voice-gateway": 'Voice Gateway',
-    "watson-assistant": 'Watson Assistant',
-    "watson-discovery": 'Watson Discovery',
-    "watson-ks": 'Watson Knowledge Studio',
-    "watson-openscale": 'IBM Watson OpenScale',
-    "watson-speech": 'Watson Speech to Text',
-    "wkc": 'Watson Knowledge Catalog',
-    "wml": 'Watson Machine Learning',
-    "wml-accelerator": 'Watson Machine Learning Accelerator',
-    "wsl": 'Watson Studio'
-}
-# Open & load the config file into a list
 i=0
 if cpak == "cp4d":
+    ############ Hard coded service list ############
+    servicescp4d = {
+        "analyticsengine": 'Analytics Engine Powered by Apache Spark',
+        "bigsql": 'Db2 Big SQL',
+        "ca": 'Cognos Analytics',
+        "cde": 'Cognos Dashboards',
+        "datagate": 'Data Gate',
+        "datastage-ent-plus": 'DataStage Enterprise Plus',
+        "db2": 'Db2',
+        "db2u": 'IBM Db2u',
+        "db2wh": 'Db2 Warehouse',
+        "dmc": 'Data Management Console',
+        "dods": 'Decision Optimization',
+        "dp": 'Data Privacy',
+        "dv": 'Data Virtualization',
+        "hadoop": 'Execution Engine for Apache Hadoop',
+        "mdm": 'IBM Master Data Management',
+        "openpages": 'OpenPages',
+        "planning-analytics": 'Planning Analytics',
+        "rstudio": 'RStudio Server',
+        "spss": 'SPSS Modeler',
+        "voice-gateway": 'Voice Gateway',
+        "watson-assistant": 'Watson Assistant',
+        "watson-discovery": 'Watson Discovery',
+        "watson-ks": 'Watson Knowledge Studio',
+        "watson-openscale": 'IBM Watson OpenScale',
+        "watson-speech": 'Watson Speech to Text',
+        "wkc": 'Watson Knowledge Catalog',
+        "wml": 'Watson Machine Learning',
+        "wml-accelerator": 'Watson Machine Learning Accelerator',
+        "wsl": 'Watson Studio'
+    }
+    # Open & load the config file into a list
     with open('cp4d-config.yaml') as f:
         list_doc = yaml.safe_load(f)
     #iterate the state of each instance in the config yaml to get installed services
@@ -97,9 +97,10 @@ if cpak == "cp4d":
 
 #####################For cp4i get installed services#####################
 
-############ Hard coded service list ############
-
-servicescp4i = {
+i=0
+if cpak == "cp4i":
+    ############ Hard coded service list ############
+    servicescp4i = {
         "platform-navigator": "Platform Navigator",
         "api-management": "API Management",
         "automation-assets": "Automation Assets",
@@ -111,11 +112,8 @@ servicescp4i = {
         "integration-design": "Integration Design",
         "integration-tracing": "Integration tracing",
         "messaging": "Messaging"
-}
-
-# Open & load the config file into a list
-i=0
-if cpak == "cp4i":
+    }
+    # Open & load the config file into a list
     with open('cp4i-config.yaml') as f:
         list_doc = yaml.safe_load(f)
     #iterate the state of each instance in the config yaml to get installed services
