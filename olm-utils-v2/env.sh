01fee4600b1ff3352ed4277d8b1498538f4f7405
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-10-06 18:53:01 +0530

    olm utils-v2 scripts
    Signed-off-by: Suyash Gupte <Suyash.Gupte2@ibm.com>

---
+++ olm-utils-v2/env.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+SERVER=
+
+# APITOKEN
+API_TOKEN=
+
+# OR
+
+# Username and password
+KUBEADMIN_USER=
+KUBEADMIN_PASS=
+
+# ICR KEY
+# Please enter the ICR KEY if the server value is pointing to IBM cloud ROKS cluster.
+ICR_KEY=
+
+
+# SCRIPT
+#Pod login and auto login to oc cluster from runutils
+if  [ -n "$KUBEADMIN_USER" ] && [ -n "$KUBEADMIN_PASS" ]
+    then
+        alias oclogin_auto="run_utils login-to-ocp -u ${KUBEADMIN_USER} -p ${KUBEADMIN_PASS} --server=${SERVER}";
+        alias pod_login="oc login -u ${KUBEADMIN_USER} -p ${KUBEADMIN_PASS} --server ${SERVER}";
+    else
+        if  [ -z "$API_TOKEN" ]
+            then
+                    echo "Invalid api token, please check env.sh file";
+            else
+                alias pod_login="oc login --token=${API_TOKEN} --server=${SERVER}";
+                alias oclogin_auto="run_utils login-to-ocp --token=${API_TOKEN} --server=${SERVER}";
+        fi
+fi
+# Pod login
+pod_login
+
+# Check if the last command executed properly
+if [ $? -eq 0 ]; then
+    echo "Logged in Successfully";
+else
+    echo "Login Failed";
+fi
+
+
+oc new-project cloud-pak-deployer
+oc project cloud-pak-deployer
+oc create serviceaccount cloud-pak-deployer-sa
+oc adm policy add-scc-to-user privileged -z cloud-pak-deployer-sa
+oc adm policy add-cluster-role-to-user cluster-admin -z cloud-pak-deployer-sa
+
+oc apply -f deployment.yaml
+
+
+waittime=0
+while [ "$pod_status" != "True" ] && [ $waittime -lt 300 ];do
+        sleep 5
+        pod_status=$(oc get po --no-headers -l deployment=cloud-pak-deployer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
+        echo "Metadata pod ready: $pod_status"
+        waittime=$((waittime+5))
+    done
+
+
+CONFIG_DIR=./cpd-config && mkdir -p $CONFIG_DIR/config
+cp cpd-config.yaml $CONFIG_DIR/config
+STATUS_DIR=./cpd-status && mkdir -p $STATUS_DIR
+
+
+DEPLOYER_POD=$(oc get po --no-headers -l deployment=cloud-pak-deployer | head -1 | awk '{print $1}')
+oc rsh $DEPLOYER_POD rm -rf /Data/cpd-config && oc cp $CONFIG_DIR $DEPLOYER_POD:/Data/cpd-config/
+
+
+oc rsh $DEPLOYER_POD  /cloud-pak-deployer/cp-deploy.sh vault set -vs cp_entitlement_key -vsv "$ICR_KEY"
+
+oc rsh $DEPLOYER_POD  /cloud-pak-deployer/cp-deploy.sh vault set -vs ibm_cp_entitlement_key -vsv "$ICR_KEY"
+
+oc rsh $DEPLOYER_POD /cloud-pak-deployer/cp-deploy.sh vault set \
+  -vs cpd-demo-oc-login -vsv "oc login --server=$SERVER --token=$API_TOKEN"
+
+oc rsh $DEPLOYER_POD /cloud-pak-deployer/cp-deploy.sh vault list
+
+# Run the deployer
+oc rsh $DEPLOYER_POD /cloud-pak-deployer/cp-deploy.sh env apply -v 
---
+++ olm-utils-v2/updateYaml.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import sys
+import os
+import yaml
+from yaml.loader import SafeLoader
+
+component=sys.argv[1]
+component_list = component.split(',')
+
+storage_vendor=sys.argv[2]
+
+# Open the file and load the file
+with open('cpd-config.yaml') as f:
+    list_doc = yaml.safe_load(f)
+
+if storage_vendor.lower()=='nfs':
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_name"]= "nfs-storage"
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_type"]= "nfs"
+
+elif storage_vendor.lower()=='ocs':
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_name"]= "ocs-storage"
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_type"]= "ocs"
+
+elif storage_vendor.lower()=='portworx':
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_name"]= "portworx-storage"
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_type"]= "portworx"
+
+elif storage_vendor.lower()=='managed-nfs-storage':
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_name"]= "nfs-storage"
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_type"]= "nfs"
+
+elif storage_vendor.lower()=='roks-classic':
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_name"] = "nfs-storage"
+    list_doc["openshift"][0]["openshift_storage"][0]["storage_type"] = "nfs"
+
+    list_doc["openshift"][0]["openshift_storage"][0]["ocp_storage_class_block"]= "ibmc-block-gold"
+    list_doc["openshift"][0]["openshift_storage"][0]["ocp_storage_class_file"] = "ibmc-file-gold-gid"
+
+i=0
+for x in range(0,len(list_doc["cp4d"][0]["cartridges"])):
+    for y in range(0,len(component_list)):
+        if list_doc["cp4d"][0]["cartridges"][x]["name"] == component_list[y]:
+            list_doc["cp4d"][0]["cartridges"][x]["state"] = "installed"
+            print(list_doc["cp4d"][0]["cartridges"][x])
+            if(y<len(component_list)):
+                x=0
+
+with open("cpd-config.yaml", "w") as f:
+    yaml.dump(list_doc, f, sort_keys=False)
