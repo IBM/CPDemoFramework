d7d0a769a658db936f5b70a96ed40a5bd0715a95
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-11-02 10:50:43 +0530

    import artifact message change techzone
    Signed-off-by: Suyash Gupte <Suyash.Gupte2@ibm.com>

--- sandbox/checkImportArtifacts.py
+++ sandbox/checkImportArtifacts.py
@@ -7,8 +7,10 @@ import json
 import os
 import copy
 
+
 disableImportGovernanceArtifacts = True
 numSuccessImportGovernanceArtifacts = 0
+availableArifacts = 3
 
 renderData = {
     "componentsToRender" : [
@@ -18,6 +20,7 @@ renderData = {
         }
     ]
 }
+
 templateJson = {
     "elementToRender": "div",
     "attributes": {
@@ -41,12 +44,14 @@ modifyCTAJSON = {
 }
 
 def enableDisableArtifactInTimeline(elementId, action):
+    global availableArifacts
     sampleJson = copy.deepcopy(templateJson)
     sampleJson["attributes"]["id"] = elementId
     if(action == "disable"):
         sampleJson["attributes"]["classList"]={
             "add": ["hidden-state"]
         }
+        availableArifacts = availableArifacts - 1
     elif(action == "enable"):
          sampleJson["attributes"]["classList"]={
             "remove": ["hidden-state"]
@@ -66,18 +71,35 @@ if not os.path.isfile("users.csv"):
     renderData["componentsToRender"][0]["dataToRender"].append(enableDisableArtifactInTimeline("task1","disable"))
 
 if os.path.isfile("governance_artifacts.zip"):
-    renderData["componentsToRender"].append(modifyCTA("task2","import-gov-artifacts",";python3.8 importGovArtifacts.py governance_artifacts.zip", numSuccessImportGovernanceArtifacts + 1))
+    numSuccessImportGovernanceArtifacts = numSuccessImportGovernanceArtifacts + 1
+    renderData["componentsToRender"].append(modifyCTA("task2","import-gov-artifacts",";python3.8 importGovArtifacts.py governance_artifacts.zip", numSuccessImportGovernanceArtifacts))
     disableImportGovernanceArtifacts = False
 
 
 if os.path.isfile("data_protection_rules.json"):
-    renderData["componentsToRender"].append(modifyCTA("task2","import-gov-artifacts",";python3.8 importDataProtectionRules.py data_protection_rules.json", numSuccessImportGovernanceArtifacts + 1))
+    numSuccessImportGovernanceArtifacts = numSuccessImportGovernanceArtifacts + 1
+    renderData["componentsToRender"].append(modifyCTA("task2","import-gov-artifacts",";python3.8 importDataProtectionRules.py data_protection_rules.json", numSuccessImportGovernanceArtifacts))
 else :
     if(disableImportGovernanceArtifacts):
         renderData["componentsToRender"][0]["dataToRender"].append(enableDisableArtifactInTimeline("task2","disable"))
 
 if not os.path.isfile("project_assets.zip"):
     renderData["componentsToRender"][0]["dataToRender"].append(enableDisableArtifactInTimeline("task3","disable"))
+
+if(availableArifacts == 0):
+    renderData["componentsToRender"].append({
+        "parentId":"artifact-message-div",
+        "dataToRender": [
+        {
+            "elementToRender": "p",
+            "attributes": {
+                "id" : "artifact-message",
+                "textContent" : "No artifacts are available for import."
+            },
+            "children": []
+        }
+    ]
+    })
 
 with open(path, "w") as outfile:
     json.dump(renderData, outfile)
