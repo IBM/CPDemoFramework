af42dff7b2086e1da580b885611a0137d9939f91
Nupur Negi <nupur@Nupurs-MacBook-Pro.local>
2023-01-31 14:24:48 +0530

    pickup oc login cmd from UI in cpdbr
    Signed-off-by: Nupur Negi <nupur@Nupurs-MacBook-Pro.local>

--- sandbox/cpdbr/configure-env.sh
+++ sandbox/cpdbr/configure-env.sh
@@ -11,23 +11,33 @@ BUCKET=$7
 REGION=$8
 ACCESS_KEY=$9
 ACCESS_ID=$10
+OC_LOGIN_COMMAND=$11
 
 # SCRIPT
 #Pod login and auto login to oc cluster from runutils
-if  [ -n "$KUBEADMIN_USER" ] && [ -n "$KUBEADMIN_PASS" ]
+#Authentication method Kube Admin
+if  [ -n "$KUBEADMIN_USER" ] && [ -n "$KUBEADMIN_PASS" ] # not null string 
     then
         alias oclogin_auto="run_utils login-to-ocp -u ${KUBEADMIN_USER} -p ${KUBEADMIN_PASS} --server=${SERVER}";
-        oc_login_command="oc login -u ${KUBEADMIN_USER} -p ${KUBEADMIN_PASS} --server ${SERVER}";
-        alias pod_login=$oc_login_command;
+        OC_LOGIN_COMMAND="oc login -u ${KUBEADMIN_USER} -p ${KUBEADMIN_PASS} --server ${SERVER}";
+        alias pod_login=$OC_LOGIN
+else
+    if  [ -z "$API_TOKEN" ]  #string has zero length
+        then
+            echo "Invalid api token, please check env.sh file";
     else
-        if  [ -z "$API_TOKEN" ]
+        #Authentication method OC Login
+        if [-n "$OC_LOGIN_COMMAND"]
             then
-                    echo "Invalid api token, please check env.sh file";
-            else
-                oc_login_command="oc login --token=${API_TOKEN} --server=${SERVER}";
-                alias pod_login=$oc_login_command;
-                alias oclogin_auto="run_utils login-to-ocp --token=${API_TOKEN} --server=${SERVER}";
+                alias pod_login=$OC_LOGIN_COMMAND;
+                alias oclogin_auto="run_utils login-to-ocp --token=${API_TOKEN} --server=${SERVER}";   
+        else
+        #Authentication method API Key
+            OC_LOGIN_COMMAND="oc login --token=${API_TOKEN} --server=${SERVER}";
+            alias pod_login=$OC_LOGIN_COMMAND
+            alias oclogin_auto="run_utils login-to-ocp --token=${API_TOKEN} --server=${SERVER}";
         fi
+    fi
 fi
 
 # Pod login
@@ -40,8 +50,7 @@ echo "success"
 
 # Store variables in shell script
 # overwrite .env file and append subsequently
-echo "OC_LOGIN_COMMAND=$oc_login_command" > .env
-echo "ICR=$ICR" >> .env
+echo "ICR=$ICR" > .env
 echo "S3_URL=$S3_URL" >> .env
 echo "BUCKET=$BUCKET" >> .env
 echo "REGION=$REGION" >> .env
@@ -51,6 +60,7 @@ echo "API_TOKEN=$API_TOKEN" >> .env
 echo "KUBEADMIN_USER=$KUBEADMIN_USER" >> .env
 echo "KUBEADMIN_PASS=$KUBEADMIN_PASS" >> .env
 echo "SERVER=$SERVER" >> .env
+echo "OC_LOGIN_COMMAND=$OC_LOGIN_COMMAND" >> .env
 
 # overwrite credentials-velero.txt file and append subsequently
 echo "[default]" > credentials-velero.txt
